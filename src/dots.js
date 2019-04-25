a5c691dcf7da05cb0468436b43effd38bcb821fd
Peter Nycander <peter.nycander@gmail.com>
2019-04-25 09:52:26 +0200

    Fix dots bug

--- src/dots.js
+++ src/dots.js
@@ -37,19 +37,57 @@ export class Dots extends React.PureComponent {
     // Credit: http://stackoverflow.com/a/13735425/1849458
     const { onMouseEnter, onMouseOver, onMouseLeave } = this.props;
     const mouseEvents = { onMouseEnter, onMouseOver, onMouseLeave };
-    var dots = Array.apply(
+    var dotsTemplate = Array.apply(
       null,
       Array(dotCount + 1)
         .join("0")
         .split("")
-    ).map((x, i) => {
-      var leftBound = i * this.props.slidesToScroll;
-      var rightBound =
-        i * this.props.slidesToScroll + (this.props.slidesToScroll - 1);
+    );
+
+    const dotsBounds = dotsTemplate.map((_, i) => {
+      const isLast = i === dotsTemplate.length - 1;
+      const isFirst = i === 0;
+      const isSecond = i === 1;
+      if (isLast) {
+        return {
+          leftBound: this.props.slideCount - this.props.slidesToScroll,
+          rightBound: Infinity
+        };
+      }
+      if (isFirst) {
+        return {
+          leftBound: 0,
+          rightBound: 0
+        }; // We only want the actually first slidesToScroll items to set first dot to current
+      }
+      if (isSecond) {
+        // Extend second due to isFirst-rules
+        return {
+          leftBound: 1,
+          rightBound: this.props.slidesToScroll
+        };
+      }
+      var leftBound =
+        this.props.slideCount -
+        (dotsTemplate.length - i) * this.props.slidesToScroll;
+      var rightBound = i * this.props.slidesToScroll;
+      return { leftBound, rightBound };
+    });
+
+    let activeIndex = 0;
+    // Make sure only one dot is active. Conflicts might occur, a decent heuristic seems to be the last one.
+    dotsBounds.forEach(({ leftBound, rightBound }, i) => {
+      if (
+        this.props.currentSlide >= leftBound &&
+        this.props.currentSlide <= rightBound
+      ) {
+        activeIndex = i;
+      }
+    });
+
+    const dots = dotsTemplate.map((_, i) => {
       var className = classnames({
-        "slick-active":
-          this.props.currentSlide >= leftBound &&
-          this.props.currentSlide <= rightBound
+        "slick-active": i === activeIndex
       });
 
       var dotOptions = {
